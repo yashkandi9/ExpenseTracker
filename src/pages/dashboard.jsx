4f36be2453a8d228a4dfb9781742796f81736a7d
TANAY SHAH <tanay@TANAYs-MacBook-Air.local>
2025-04-28 17:00:28 +0530

    landing push2

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,14 @@
 import React, { useState } from 'react';
+import Lottie from "react-lottie"
+import animationData from '../assets/a.json';
+import { SiConvertio } from "react-icons/si";
+import { RiStockLine } from "react-icons/ri";
+import { RiMoneyRupeeCircleFill } from "react-icons/ri";
+import { FaLightbulb } from "react-icons/fa6";
+
+
+
+
 
 const Dashboard = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -7,13 +17,24 @@ const Dashboard = () => {
     setIsLoggedIn(!isLoggedIn);
   };
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <div className="dashboard">
         
         <div className='landing'>
            
             <h1>Personal Finance Dashboard</h1>
-            <img src="/dash.png" alt="Dashboard"/>
+            <div style={{ width: isLoggedIn ? "300px" : "400px" }}>
+              <Lottie options={defaultOptions} />
+            </div>
         </div>
 
       <header className="header">
@@ -34,22 +55,25 @@ const Dashboard = () => {
         {isLoggedIn ? (
           <>
             
-
             <div className="options">
               <div className="option">
                 <h2>Expense Tracker</h2>
+                <RiMoneyRupeeCircleFill size={30} color="#5b4bc4"/>
                 <button>Go to Expense Tracker</button>
               </div>
               <div className="option">
                 <h2>Currency Converter</h2>
+                <SiConvertio size={30} color="#5b4bc4"/>
                 <button>Go to Currency Converter</button>
               </div>
               <div className="option">
                 <h2>Stock Analyzer</h2>
+                <RiStockLine size={30} color="#5b4bc4" />
                 <button>Go to Stock Analyzer</button>
               </div>
               <div className="option">
                 <h2>Quizify</h2>
+                <FaLightbulb size={30} color="#5b4bc4"/>
                 <button>Start Quiz</button>
               </div>
             </div>
@@ -63,3 +87,9 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+
+
+
+// React Emoticons
+// Clerk (login sign up page)
